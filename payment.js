9a59df893ae7776e75402a751f92e17bc0f557cb
West <arukpesolomon@gmail.com>
2022-11-08 08:24:00 -0500

    added esle to payment

--- payment.js
+++ payment.js
@@ -3,4 +3,6 @@ let oldPayment = 5000;
 
 if (newPayment > oldPayment) {
     console.log( 'You have balance in your account of ${newPayment - oldPayment}' );
-    }
+    } else {
+        console.log( 'You have no balance in your account' );
+}
